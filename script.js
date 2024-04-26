ff96fea89ad05eb481e61844cf315e82785afa9d
Mariam J. Hussein <engmhussein86@gmail.com>
2024-04-26 13:58:07 -0400

    add static

--- script.js
+++ script.js
@@ -30,9 +30,12 @@ const adventurer = {
     adventurer.roll(1);
 
     class Character {
+        static MAX_HEALTH  = 100;
+
+
         constructor (name) {
           this.name = name;
-          this.health = 100;
+          this.health = Character.MAX_HEALTH;
           this.inventory = [];
         }
 
@@ -45,12 +48,59 @@ const adventurer = {
       }
 
 
-      const robin = new Character("Robin");
-robin.inventory = ["sword", "potion", "artifact"];
-robin.companion = new Character("Leo");
-robin.companion.type = "Cat";
-robin.companion.companion = new Character("Frank");
-robin.companion.companion.type = "Flea";
-robin.companion.companion.inventory = ["small hat", "sunglasses"];
+//       const robin = new Character("Robin");
+// robin.inventory = ["sword", "potion", "artifact"];
+// robin.companion = new Character("Leo");
+// robin.companion.type = "Cat";
+// robin.companion.companion = new Character("Frank");
+// robin.companion.companion.type = "Flea";
+// robin.companion.companion.inventory = ["small hat", "sunglasses"];
 
-robin.roll(1);
+// robin.roll(1);
+
+
+class Adventurer extends Character {
+    static ROLES = ["Fighter", "Healer", "Wizard"];
+    constructor (name, role) {
+      super(name);
+      if (!Adventurer.ROLES.includes(role)) {
+        throw new Error(`Invalid role '${role}' for adventurer.`);
+      }
+      // Adventurers have specialized roles.
+      this.role = role;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  }
+
+
+  class Companion  extends Character {
+    constructor (name, type) {
+      super(name);
+      // Adventurers have specialized roles.
+      this.type = type;
+      // Every adventurer starts with a bed and 50 gold coins.
+      this.inventory.push("bedroll", "50 gold coins");
+    }
+    // Adventurers have the ability to scout ahead of them.
+    scout () {
+      console.log(`${this.name} is scouting ahead...`);
+      super.roll();
+    }
+  }
+
+  const robin = new Adventurer("Robin","Fighter");
+//   robin.inventory = ["sword", "potion", "artifact"];
+  robin.companion = new Companion("Leo","Cat");
+  robin.companion.companion = new Companion("Frank","Flea");
+  robin.companion.companion.inventory = ["small hat", "sunglasses"];
+
+  console.log(robin);
+
+  
+
